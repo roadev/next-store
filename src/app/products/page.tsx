ad96a761ae6e573551e0e5503b1dbe6a89f5510d
Juan Roa <hello@juanroa.dev>
2025-04-03 14:40:26 -0500

    fixed product navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { useProducts } from "@/hooks/useProducts";
+import { fetcher } from "@/lib/fetcher";
+import Link from "next/link";
+import { useQueryClient } from "@tanstack/react-query";
+import { Product } from "@/types/product";
 
-export default function ProductosPage() {
+export default function ProductsPage() {
   const { data, isLoading, isError } = useProducts();
+  const queryClient = useQueryClient();
 
   if (isLoading)
     return <p className="text-center mt-10">Loading products...</p>;
@@ -18,19 +23,31 @@ export default function ProductosPage() {
     <main className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">Product Catalog</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data.map((product) => (
-          <div
+        {data.map((product: Product) => (
+          <Link
             key={product.id}
-            className="border rounded-lg p-4 hover:shadow-md transition"
+            href={`/products/${product.id}`}
+            onMouseEnter={() =>
+              queryClient.prefetchQuery({
+                queryKey: ["product", String(product.id)],
+                queryFn: () =>
+                  fetcher<Product>(
+                    `https://fakestoreapi.com/products/${product.id}`,
+                  ),
+              })
+            }
+            className="cursor-pointer"
           >
-            <img
-              src={product.image}
-              alt={product.title}
-              className="h-40 w-full object-contain mb-4"
-            />
-            <h2 className="font-semibold line-clamp-2">{product.title}</h2>
-            <p className="mt-2 font-bold">${product.price}</p>
-          </div>
+            <div className="border rounded-lg p-4 hover:shadow-md transition h-full flex flex-col">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-40 w-full object-contain mb-4"
+              />
+              <h2 className="font-semibold line-clamp-2">{product.title}</h2>
+              <p className="mt-auto font-bold">${product.price}</p>
+            </div>
+          </Link>
         ))}
       </div>
     </main>
