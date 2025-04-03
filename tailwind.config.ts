249e9b616f8066e62241783a221c7d3cdf0094cb
Juan Roa <hello@juanroa.dev>
2025-04-02 23:38:41 -0500

    tailwind config and theme

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0f172a",
+        secondary: "#facc15",
+        accent: "#6366f1",
+      },
+    },
+  },
+  plugins: [],
+};
